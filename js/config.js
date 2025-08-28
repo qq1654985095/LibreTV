32eaf7b6ffbf8a2d292fb91fa8127dc68b186936
qq1654985095 <tuyiqiang2008@163.com>
2025-08-28 12:59:23 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,20 +22,6 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    dyttzy: {
-    api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-    name: '电影天堂资源',
-	},
-    ruyi: {
-    api: 'https://cj.rycjapi.com/api.php/provide/vod',
-    name: '如意资源',
-	},
-	
-	lzi: {
-    api: 'https://cj.lziapi.com/api.php/provide/vod/',
-    name: '量子资源站'
-
-  },
 
   
 };
